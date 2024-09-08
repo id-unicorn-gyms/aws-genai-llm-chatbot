d3ebbb935f910d59d53410b310822a05bbd1e0cc
ariefh2-at-320740598534 <mr.arief.hidayat@gmail.com>
2024-09-09 05:12:45 +0700

    workaround: add JumpStartConstants.JUMPSTART_LAUNCHED_REGIONS for ap-southeast-3

--- bin/aws-genai-llm-chatbot.ts
+++ bin/aws-genai-llm-chatbot.ts
@@ -5,10 +5,17 @@ import { AwsGenAILLMChatbotStack } from "../lib/aws-genai-llm-chatbot-stack";
 import { AwsSolutionsChecks } from "cdk-nag";
 import { getConfig } from "./config";
 import { Aspects } from "aws-cdk-lib";
+import { JumpStartConstants } from "@cdklabs/generative-ai-cdk-constructs/lib/patterns/gen-ai/aws-model-deployment-sagemaker/private/jumpstart-constants";
 
 const app = new cdk.App();
 
 const config = getConfig();
+
+// TODO: fix https://github.com/awslabs/generative-ai-cdk-constructs/blob/main/src/patterns/gen-ai/aws-model-deployment-sagemaker/private/jumpstart-constants.ts
+JumpStartConstants.JUMPSTART_LAUNCHED_REGIONS["ap-southeast-3"] = {
+  contentBucket: "jumpstart-cache-prod-ap-southeast-3",
+  gatedContentBucket: "jumpstart-private-cache-prod-ap-southeast-3",
+};
 
 new AwsGenAILLMChatbotStack(app, `${config.prefix}GenAIChatBotStack`, {
   config,
