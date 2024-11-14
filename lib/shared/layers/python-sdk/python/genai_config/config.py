9ae2e1f4e8e3a996f925c01b5a6512985908aece
khairul habib <mkhabib@amazon.id>
2024-11-14 20:15:31 +0700

    add import utils

--- lib/shared/layers/python-sdk/python/genai_config/config.py
+++ lib/shared/layers/python-sdk/python/genai_config/config.py
@@ -1,5 +1,7 @@
 # create a class that call dynamodb/utils.py to read value from key
 from typing import Any
+from genai_config.dynamodb.utils import read_key_value_from_dynamodb
+
 class DynamoDBReader:
     def __init__(self, table_name: str, key_name: str):
         self.table_name = table_name
